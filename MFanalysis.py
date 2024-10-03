cd427d55404b6e24cd459e344056ca458eb855b6
Benjamin Rathman <2021rathmabe@gmail.com>
2024-10-03 16:11:54 -0400

    added imports in analysis file

--- MFanalysis.py
+++ MFanalysis.py
@@ -1,1 +1,4 @@
 #this is where we will do the analysis of the stock
+from sklearn.model_selection import train_test_split # type: ignore
+import pandas as pd # type: ignore
+from sklearn.linear_model import LogisticRegression # type: ignore
